88d1ddb8c263840da7d63eb2eb6995f874e1f8cd
黄敏 <huangmin0822@163.com>
2015-06-03 18:20:43 +0800

    add welcome

--- config/routes.rb
+++ config/routes.rb
@@ -1,49 +1,9 @@
 # -*- encoding : utf-8 -*-
 Rails.application.routes.draw do
-  root 'blogs#index'
+  root 'welcome#index'
 
   resources :articles
   resources :blogs ,only: [:index,:show]
 
-
   mount Ckeditor::Engine => '/ckeditor'
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
 end
